bd7bfc18860aafa9d14dd623e495fd462a1bb8a4
dmeurisse <info@mchobby.be>
2020-05-20 23:53:38 +0200

    RGB to HVS (vice-versa) coming from syscolors

--- COLORS/lib/colortls.py
+++ COLORS/lib/colortls.py
@@ -8,3 +8,50 @@ def charpix( r,g,b ):
 	    Would allow to display rudimentary picture on terminal """
 	pc = int( grayscale(r,g,b) / 25.5 * 10 ) # In percent for a range 0..255
 	return "  ..-+**XXX"[pc//10]
+
+# From ColorSys @ https://raw.githubusercontent.com/python/cpython/2.7/Lib/colorsys.py
+# HSV: Hue, Saturation, Value
+# H: position in the spectrum
+# S: color saturation ("purity")
+# V: color brightness
+def rgb_to_hsv(r, g, b):
+	maxc = max(r, g, b)
+	minc = min(r, g, b)
+	v = maxc
+	if minc == maxc:
+		return 0.0, 0.0, v
+	s = (maxc-minc) / maxc
+	rc = (maxc-r) / (maxc-minc)
+	gc = (maxc-g) / (maxc-minc)
+	bc = (maxc-b) / (maxc-minc)
+	if r == maxc:
+		h = bc-gc
+	elif g == maxc:
+		h = 2.0+rc-bc
+	else:
+		h = 4.0+gc-rc
+	h = (h/6.0) % 1.0
+	return h, s, v
+
+def hsv_to_rgb(h, s, v):
+	if s == 0.0:
+		return v, v, v
+	i = int(h*6.0) # XXX assume int() truncates!
+	f = (h*6.0) - i
+	p = v*(1.0 - s)
+	q = v*(1.0 - s*f)
+	t = v*(1.0 - s*(1.0-f))
+	i = i%6
+	if i == 0:
+		return v, t, p
+	if i == 1:
+		return q, v, p
+	if i == 2:
+		return p, v, t
+	if i == 3:
+		return p, q, v
+	if i == 4:
+		return t, p, v
+	if i == 5:
+		return v, p, q
+	# Cannot get here
