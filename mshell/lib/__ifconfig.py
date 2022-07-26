32afb14b769b0034cdcd021b3981d2d7650d9f06
dmeurisse <info@mchobby.be>
2022-07-26 11:10:08 +0200

    ifconfig: show info for AP mode

--- mshell/lib/__ifconfig.py
+++ mshell/lib/__ifconfig.py
@@ -12,6 +12,13 @@ def ifconfig( shell, args ):
 	ap = network.WLAN( network.AP_IF )
 
 	shell.println( "Access Point: %s" % ('ACTIVE' if ap.active() else 'inactive') )
+	if ap.active():
+		shell.println( "   %r" % sta )
+		_ip, _mask, _gateway, _dns = ap.ifconfig()
+		shell.println( "   IP     : %s" % _ip )
+		shell.println( "   NetMask: %s" % _mask )
+		shell.println( "   Gateway: %s" % _gateway )
+		shell.println( "   DNS    : %s" % _dns )
 	shell.println( "Station mode: %s" % ('ACTIVE' if sta.active() else 'inactive') )
 	if sta.active():
 		shell.println( "   %r" % sta )
