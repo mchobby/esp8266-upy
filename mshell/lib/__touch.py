0c29f15872f88e11707ed581f1c0855977a25311
dmeurisse <info@mchobby.be>
2022-08-02 01:46:10 +0200

    move mshell to micropython-minishell repository

--- mshell/lib/__touch.py
+++
@@ -1,9 +0,0 @@
-""" MiniShell touch command. create a file """
-
-def touch( shell, args ):
-	if len(args)<1:
-		shell.println( "arg required!")
-		return 1
-	_file = open( args[0], "w" )
-	_file.close()
-	return 0
--- mshell/lib/bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Download dependencies again..."
-wget -N https://raw.githubusercontent.com/robert-hh/Micropython-Editor/master/pye.py
