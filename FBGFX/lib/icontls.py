1b1c8810519160a8dfd937a053d5b945289f925f
dmeurisse <info@mchobby.be>
2026-06-01 02:08:52 +0200

    draw icon larger that 8 pixels

--- FBGFX/lib/icontls.py
+++ FBGFX/lib/icontls.py
@@ -1,10 +1,12 @@
+__version__ = '0.2.0'
+
 def draw_icon( fb, icon, x, y, color ):
 	""" Display one of the icon in the fb FrameBuffer @ x,y position with the C color.
 		Icons are stored into the icons.py file. Only draws pixels (don't clear pixels) """
 	size = icon[0]
 	for row in range( size ):
 		for col in range( size ):
-			if (icon[row+1] & (1<<(7-col))) > 0:
+			if (icon[row+1] & (1<<(size-1-col))) > 0:
 				fb.pixel(col+x,row+y,color)
 
 def icon_as_list( icon ):
@@ -14,7 +16,7 @@ def icon_as_list( icon ):
 	for row in range( size ):
 		_line = list()
 		for col in range( size ):
-			_line.append( (icon[row+1] & (1<<(7-col))) > 0 )
+			_line.append( (icon[row+1] & (1<<(size-1-col))) > 0 )
 		_r.append( _line )
 	return _r
 
@@ -25,6 +27,6 @@ def icon_as_text( icon ):
 	for row in range( size ):
 		_s = ''
 		for col in range( size ):
-			_s += '*' if (icon[row+1] & (1<<(7-col))) > 0 else ' '
+			_s += '*' if (icon[row+1] & (1<<(size-1-col))) > 0 else ' '
 		_r.append( _s )
 	return _r
